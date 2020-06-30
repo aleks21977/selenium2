ce3ea6cc7a4337faac82874311fdc3c38aec682b
aleks21977 <35885304+aleks21977@users.noreply.github.com>
2020-06-30 21:02:27 +0300

    DZ7

--- src/test/java/ru/stqa/selenium/MyFirstTest.java
+++ src/test/java/ru/stqa/selenium/MyFirstTest.java
@@ -5,10 +5,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
@@ -21,18 +28,25 @@ public class MyFirstTest {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        FirefoxOptions options = new FirefoxOptions().setLegacy(true);
+        options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox ESR\\firefox.exe")));
+        WebDriver driver = new FirefoxDriver(options);
+
+
+//        DesiredCapabilities caps = new DesiredCapabilities();
+//        //caps.setCapability(FirefoxDriver.MARIONETTE, false);
+//        WebDriver driver = new FirefoxDriver(
+//                new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox ESR\\firefox.exe")),
+//                new FirefoxProfile(), caps);
+//        System.out.println(((HasCapabilities) driver).getCapabilities());
+//        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 3);
+
     }
 
     @Test
     public void myFirstTest() {
         driver.get("https://www.google.com/");
-        //driver.findElement(By.id("gs_ok2")).click();
-        //driver.findElement(By.id("K32")).click();
-        //driver.findElement(By.id("gs_ok2")).click();
-        //driver.findElement(By.id("K32")).click();
         driver.findElement(By.name("q")).sendKeys("webdriver");
         driver.findElement(By.name("btnK")).click();
         wait.until(titleIs("webdriver - Поиск в Google"));
---
+++ src/test/java/ru/stqa/selenium/TestBase.java
@@ -0,0 +1,50 @@
+package ru.stqa.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidSelectorException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public boolean isElementPresent (By locator) {
+        try {
+            //wait.until((WebDriver d) -> d.findElement(locator)); //явное ожидание l3_m9 6:30
+            driver.findElement(locator); //не явное ожидание l3_m9 6:30
+            return true;
+        } catch (NoSuchElementException ex) { //для явного необходимо исправить l3_m9 6:30
+            return false;
+        }
+    }
+
+    public boolean areElementsPresent (By locator) {
+        try {
+            return driver.findElements(locator).size() > 0;
+        } catch (InvalidSelectorException ex) {
+            return false;
+        }
+    }
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 5);
+    }
+
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
