80bb7d122c75e5cdc4fecf00ceb7d3b9da5841ca
aleks21977 <35885304+aleks21977@users.noreply.github.com>
2020-07-26 02:49:59 +0300

    DZ17

--- src/test/java/ru/stqa/selenium/DZ17.java
+++ src/test/java/ru/stqa/selenium/DZ17.java
@@ -12,6 +12,8 @@ import static org.junit.Assert.assertTrue;
 
 public class DZ17 extends TestBase {
 
+    public ArrayList<String> allCategoryIds = new ArrayList<String>();
+
     @Test
     public void dz17Test() {
         driver.get("http://localhost/litecart/admin/");
@@ -19,75 +21,94 @@ public class DZ17 extends TestBase {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
         driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=0/");
+        clickGoods("0"); //прокликивам все товары подкатегории
+
+        ArrayList<String> firstCategoryIds = new ArrayList<String>();
 
         //******************проверка есть ли вложенные подкатегории
+        checkIncludeCategories();
+
+        //*************прокликиваем подкатегории
+        klickCategories(firstCategoryIds);
+    }
+
+
+    private void klickCategories(ArrayList<String> firstCategoryIds) {
+
+        //*************прокликиваем подкатегории
+        for (String categoryId : firstCategoryIds) {
+            try { Thread.sleep(1000); } catch (Exception e) { }//пауза
+            driver.findElement(By.xpath("//a[@href[contains(.,'catalog&category_id=" + categoryId + "')]]")).click();
+
+            clickGoods(categoryId); //прокликивам все товары подкатегории
+
+            checkIncludeCategories(); //проверка есть ли вложенные подкатегории
+        }
+    }
+
+
+    private void clickGoods(String categoryId) {
+        //*****************получаем список всех товаров подкатегории
+        List<WebElement> listGoodsCategories = driver.findElements
+                (By.xpath("//table[@class='dataTable']//td[3]/a[@href[contains(.,'category_id=" + categoryId + "&product_id=')]]"));
+
+        //******получаем у всех товаров в подкатегории Id
+        ArrayList<String> goodsIds = new ArrayList<String>();
+        for (WebElement goodsCategory : listGoodsCategories) {
+            String hrefGoods = goodsCategory.getAttribute("href");
+            goodsIds.add(substringAfter(hrefGoods, "product_id="));
+        }
+
+        //***********прокликиваем все товары подкатегории
+        for (String goodsId : goodsIds) {
+            try { Thread.sleep(1000); } catch (Exception e) { }//пауза
+            driver.findElement(By.xpath("//a[@href[contains(.,'category_id=" + categoryId + "&product_id=" + goodsId + "')]]")).click();
+            try { Thread.sleep(1900); } catch (Exception e) { }//пауза
+
+            //***********проверяем что на консоль ничего не выодится
+            driver.manage().logs().get("browser").forEach(l -> System.out.println(l));
+            List haveLogsString = driver.manage().logs().get("browser").getAll();
+            System.out.println("haveLogsString=" + haveLogsString);
+            assertTrue(haveLogsString.size() == 0);
+            driver.findElement(By.name("cancel")).click();
+        }
+    }
+
+
+    public void checkIncludeCategories() {
+
+        //******************проверка есть ли вложенные подкатегории и если подкатегории есть то получаем их
         if (areElementsPresent(By.xpath("//table[@class='dataTable']//td[3]/a[@href[contains(.,'catalog&category_id')]]"))) {
+
             //****************получаем все подкатегории
             List<WebElement> countSubCategories = driver.findElements
                     (By.xpath("//table[@class='dataTable']//td[3]/a[@href[contains(.,'catalog&category_id')]]"));
+
             //************получаем у всех подкатегорий id
-            ArrayList<String> categoryIds = new ArrayList<String>();
+            ArrayList<String> categoryIds = new ArrayList<String>();//массив для прокликывания подкатегорий
             for (WebElement subCategory : countSubCategories) {
                 String hrefCategory = subCategory.getAttribute("href");
-                categoryIds.add(substringAfter(hrefCategory, "catalog&category_id="));
+
+                //проверяем есть ли категория в массиве и если нет то добавляем ее в массив
+                String getId = substringAfter(hrefCategory, "catalog&category_id=");
+                if (checkIsContainsId(allCategoryIds, getId)) { //если их нет то добавляем
+                    allCategoryIds.add(getId);
+                    categoryIds.add(getId);
+                }
             }
 
             //*************прокликиваем подкатегории
-            for (String categoryId : categoryIds) {
-                try { Thread.sleep(1500); } catch (Exception e) { }//пауза
-                driver.findElement(By.xpath("//a[@href[contains(.,'catalog&category_id=" + categoryId + "')]]")).click();
-                //*****************получаем список всех товаров подкатегории
-                List<WebElement> listGoodsCategories = driver.findElements
-                        (By.xpath("//table[@class='dataTable']//td[3]/a[@href[contains(.,'category_id=" + categoryId + "&product_id=')]]"));
-                //******получаем у всех товаров в подкатегории Id
-                ArrayList<String> goodsIds = new ArrayList<String>();
-                for (WebElement goodsCategory : listGoodsCategories) {
-                    String hrefGoods = goodsCategory.getAttribute("href");
-                    goodsIds.add(substringAfter(hrefGoods, "product_id="));
-                }
-                //System.out.println(goodsIds);
-
-                //***********прокликиваем все товары подкатегории
-                for (String goodsId : goodsIds) {
-                    try { Thread.sleep(1000); } catch (Exception e) { }//пауза
-                    driver.findElement(By.xpath("//a[@href[contains(.,'category_id=" + categoryId + "&product_id=" + goodsId + "')]]")).click();
-                    try { Thread.sleep(1000); } catch (Exception e) { }//пауза
-                    //***********проверяем что на консоль ничего не выодится
-                    driver.manage().logs().get("browser").forEach(l -> System.out.println(l));
-                    List haveLogsString = driver.manage().logs().get("browser").getAll();
-                    System.out.println("haveLogsString=" + haveLogsString);
-                    assertTrue(haveLogsString.size() == 0);
-                    driver.findElement(By.name("cancel")).click();
-                }
-            }
+            klickCategories(categoryIds);
         }
+    }
 
 
-
-
-        
-        //******получаем Id карневой категории
-
-
-        //******получаем Id товаров карневой категории
-//        ArrayList<String> goodsIdsWithRoot = new ArrayList<String>();
-//        for (WebElement goodsCategory : listGoodsRootCategories) {
-//            String hrefGoods = goodsCategory.getAttribute("href");
-//            goodsIdsWithRoot.add(substringAfter(hrefGoods, "product_id="));
-//        }
-//        //System.out.println(goodsIds);
-//
-//        //***********прокликиваем все товары подкатегории
-//        for (String goodsId : goodsIdsWithRoot) {
-//            driver.findElement(By.xpath("//a[@href[contains(.,'category_id=" + categoryId + "&product_id=" + goodsId + "')]]")).click();
-//            try { Thread.sleep(1000); } catch (Exception e) { }//пауза
-//            //***********проверяем что на консоль ничего не выодится
-//            driver.manage().logs().get("browser").forEach(l -> System.out.println(l));
-//            List haveLogsString = driver.manage().logs().get("browser").getAll();
-//            System.out.println("haveLogsString=" + haveLogsString);
-//            assertTrue(haveLogsString.size() == 0);
-//            driver.findElement(By.name("cancel")).click();
-//        }
-        try { Thread.sleep(3000); } catch (Exception e) { }//пауза
+    //проверка есть ли id в массиве
+    public boolean checkIsContainsId(ArrayList<String> allCategoryIds, String Id) {
+        if (allCategoryIds.contains(Id)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
